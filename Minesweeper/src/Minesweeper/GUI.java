23c8816b58bc7d9ba96b8e7f58d804c8e826995b
Đỗ Duy Anh <outtavayne@gmail.com>
2024-11-26 08:00:30 +0700

    update : flagger + size adjust buttons

--- Minesweeper/src/Minesweeper/GUI.java
+++ Minesweeper/src/Minesweeper/GUI.java
@@ -10,24 +10,39 @@ import javax.swing.*;
 
 public class GUI extends JFrame {
 	public boolean resetter = false;
+    public boolean flagger = false;
 	Date startDate = new Date();
-    int spacing = 5;
+    Date endDate;
+    int spacing = 4;
     int neighs = 0;
+    String vicMes = " Nothing yet!";
     public int mx = -100;
     public int my = -100;
     public int smileyX=605;
     public int smileyY=5;
     public int smileyCenterX = smileyX+35;
     public int smileyCenterY = smileyY+35;
+    public int flaggerX = 445;
+    public int flaggerY = 5;
+    public int flaggerCenterX = flaggerX + 35;
+    public int flaggerCenterY = flaggerY + 35;
+    public int spacingX = 90;
+    public int spacingY = 10;
+    public int minusX = spacingX+160;
+    public int minusY = spacingY;
+    public int plusX = spacingX +240;
+    public int plusY = spacingY;
     public int timeX=1130;
     public int timeY=5;
+    public int vicMesX = 740;
+    public int vicMesY= -50;
     public int sec = 0;
     public boolean happiness = true;
     public boolean victory = false;
     public boolean defeat = false;
     int[][] mines = new int[16][9];
     int[][] neighbours = new int[16][9];
-    boolean[][] releaved = new boolean[16][9];
+    boolean[][] revealed = new boolean[16][9];
     boolean[][] flagged = new boolean[16][9];
     Random rand = new Random();
     public GUI() {
@@ -53,7 +68,7 @@ public class GUI extends JFrame {
         		} else {
         			mines[i][j]=0;
         		}
-        		releaved[i][j]=false;
+        		revealed[i][j]=false;
         	}
         for(int i=0;i<16;i++)
         	for(int j=0;j<9;j++) {
@@ -74,9 +89,9 @@ public class GUI extends JFrame {
             g.setColor(Color.DARK_GRAY);
             g.fillRect(0, 0, 1280, 800);
             for (int i = 0; i < 16; i++)
-                for (int j = 0; j < 9; j++) {
+              {  for (int j = 0; j < 9; j++) {
                     g.setColor(Color.GRAY);
-                    if (releaved[i][j] == true) {
+                    if (revealed[i][j] == true) {
                     	g.setColor(Color.WHITE);
                     	if (mines[i][j]==1) {
                     		g.setColor(Color.red);
@@ -87,7 +102,7 @@ public class GUI extends JFrame {
                         g.setColor(Color.lightGray);
                     }
                     g.fillRect(spacing + i * 80, spacing + j * 80 + 80, 80 - 2 * spacing, 80 - 2 * spacing);
-                    if (releaved[i][j] == true) {
+                    if (revealed[i][j] == true) {
                     	g.setColor(Color.BLACK);
                     	if(mines[i][j] == 0 && neighbours[i][j] != 0) {
                     		if(neighbours[i][j]==1) {
@@ -117,7 +132,43 @@ public class GUI extends JFrame {
                     	g.fillRect(i*80+15, j*80+118, 50, 4);
                     }
                 }
+                    if (flagged[i][j]==true)
+                    { g.setColor(Color.BLACK);
+                        g.fillRect(i*80 +32+5,j*80+80 +15+5,5,40);
+                        g.fillRect(i*80 +20+5,j*80+80 +50+5,30,10);
+                        g.setColor(Color.RED);
+                        g.fillRect(i*80+16+5,j*80+80+15+5,20,15);
+                        g.setColor(Color.BLACK);
+                        g.drawRect(i*80+16+5,j*80+80+15+5,20,15);
+                        g.drawRect(i*80+17+5,j*80+80+16+5,18,13);
+                        g.drawRect(i*80+18+5,j*80+80+17+5,16,11);
+                    }
            }
+        }
+            g.setColor(Color.BLACK);
+            g.fillRect(spacingX,spacingY,300,60);
+
+            g.setColor(Color.white);
+            g.fillRect(minusX+5,minusY+10,40,40);
+            g.fillRect(plusX+5,plusY+10,40,40);
+        
+            g.setFont(new Font("Tahoma",Font.PLAIN,35));
+            g.drawString(" Spacing", spacingX+20, spacingY+45);
+         
+            g.setColor(Color.BLACK);
+            g.fillRect(minusX+15, minusY+27,20 ,6 );
+            g.fillRect(plusX+15, plusY+27,20 ,6 );
+            g.fillRect(plusX+22, plusY+20,6 ,20 );
+            
+            g.setColor(Color.white);
+            g.setFont(new Font("Tahoma",Font.PLAIN,30));
+            if(spacing<10)
+            {g.drawString("0"+Integer.toString(spacing),minusX+50,minusY+40);}
+            else{
+            g.drawString(Integer.toString(spacing),minusX+50,minusY+40);}
+
+          
+
             g.setColor(Color.yellow);
             g.fillOval(smileyX, smileyY, 70, 70);
             g.setColor(Color.black);
@@ -132,10 +183,27 @@ public class GUI extends JFrame {
             	g.fillRect(smileyX+17, smileyY+50, 5, 5);
             	g.fillRect(smileyX+48, smileyY+50, 5, 5);
             }
+            g.setColor(Color.BLACK);
+            g.fillRect(flaggerX +32,flaggerY +15,5,40);
+            g.fillRect(flaggerX +20,flaggerY +50,30,10);
+            g.setColor(Color.RED); 
+            g.fillRect(flaggerX+16,flaggerY+15,20,15);
+            g.setColor(Color.BLACK);
+            g.drawRect(flaggerX+16,flaggerY+15,20,15);
+            g.drawRect(flaggerX+17,flaggerY+16,18,13);
+            g.drawRect(flaggerX+18,flaggerY+17,16,11);
             
+            
+            if (flagger == true)
+            {g.setColor(Color.red);
+                            }
+            g.drawOval(flaggerX,flaggerY,70,70);
+            g.drawOval(flaggerX+1,flaggerY+1,68,68);
+            g.drawOval(flaggerX+2,flaggerY+2,66,66);
             g.setColor(Color.BLACK);
             g.fillRect(timeX,timeY,150,70);
-            sec = (int)((new Date().getTime()-startDate.getTime())/1000);
+            if (defeat == false && victory == false)
+{            sec = (int)((new Date().getTime()-startDate.getTime())/1000);}
             if (sec>999)
             	sec=999;
             g.setColor(Color.WHITE);
@@ -152,6 +220,18 @@ public class GUI extends JFrame {
             } else {
             	g.drawString(Integer.toString(sec), timeX, timeY+65);
             }
+
+            if (victory==true){g.setColor(Color.GREEN);
+                vicMes = "You win!";}
+
+            else if  (defeat==true){g.setColor(Color.RED);
+                vicMes = "You lose!";}
+
+        if (victory == true || defeat == true)
+            {vicMesY= -50 + (int)(new Date().getTime() - endDate.getTime())/10;}
+           if(vicMesY>67){vicMesY=67;}
+            g.setFont(new Font ("Tahoma",Font.PLAIN,70));
+            g.drawString(vicMes,vicMesX,vicMesY);
         }
     }
 
@@ -174,14 +254,49 @@ public class GUI extends JFrame {
 
         @Override
         public void mouseClicked(MouseEvent e) {
-        	if(inBoxX() != -1 && inBoxY() != -1) {
-        		releaved[inBoxX()][inBoxY()]=true;
-        		System.out.println("[" + inBoxX() + "," + inBoxY() +"], neighs "+neighbours[inBoxX()][inBoxY()]);
-        	}
+        	mx = e.getX();
+            my = e.getY();
+    
+         
+            if (mx >= minusX + 20 && mx <= minusX + 60 && my >= minusY + 20 && my <= minusY + 50) {
+                spacing--;
+                if (spacing < 1) {
+                    spacing = 1;
+                }
+    
+            }
+         
+            else if (mx >= plusX + 20 && mx <= plusX + 60 && my >= plusY + 20 && my <= plusY + 50) {
+                spacing++;
+                if (spacing > 15) {
+                    spacing = 15;
+                }
+            }
+            
+        
+
+            if(inBoxX() != -1 && inBoxY() != -1) {
+                System.out.println("The mouse is in the [" + inBoxX() + "],Number of mine neighs:"+neighbours[inBoxX()][inBoxY()]);
+                if(flagger == true && revealed[inBoxX()][inBoxY()] == false ){
+                    if( flagged[inBoxX()][inBoxY()]== false)
+                  {flagged[inBoxX()][inBoxY()]= true;}
+                  else { flagged[inBoxX()][inBoxY()]=false;}}
+                  else {if(flagged[inBoxX()][inBoxY()]==false)
+                   { revealed[inBoxX()][inBoxY()]=true;}}
+                }
+            else {System.out.println("The pointer is not inside of any box!");}
+
         	if(inSmiley() == true) {
         		resetAll();
         	}
-        }
+          
+        	if(inFlagger() == true) {
+                if(flagger == false){
+        	flagger=true;}
+            else{ flagger = false;}
+        	}
+
+    } 
 
         @Override
         public void mousePressed(MouseEvent e) {
@@ -206,15 +321,20 @@ public class GUI extends JFrame {
     }
     
     public void checkVictoryStatus() {
+        if(defeat == false){
     	for(int i=0;i<16;i++)
-    		for(int j=0;j<9;j++) {
-    			if(releaved[i][j] == true && mines[i][j]==1) {
+    		{for(int j=0;j<9;j++) {
+    			if(revealed[i][j] == true && mines[i][j]==1) {
     				defeat = true;
     				happiness = false;
+                    endDate = new Date();
     			}
     		}
-    	if(totalBoxesRevealed()-totalMines()>=144) {
+        }
+        }
+    	if(totalBoxesRevealed()>=144-totalMines()&&victory == false) {
     		victory = true;
+            endDate = new Date();
     	}
     }
     
@@ -232,7 +352,7 @@ public class GUI extends JFrame {
     	int total =0;
     	for (int i=0;i<16;i++)
     		for(int j=0;j<9;j++) {
-    			if(releaved[i][j]==true) 
+    			if(revealed[i][j]==true) 
     				total++;
     		}
     	return total;
@@ -240,6 +360,10 @@ public class GUI extends JFrame {
     
     public void resetAll() {
     	resetter = true;
+        flagger = false;
+        startDate = new Date();
+        vicMesY= -50;
+        vicMes = " Nothing yet!";
     	happiness = true;
     	victory = false;
     	defeat = false;
@@ -251,7 +375,7 @@ public class GUI extends JFrame {
         		} else {
         			mines[i][j]=0;
         		}
-        		releaved[i][j]=false;
+        		revealed[i][j]=false;
         		flagged[i][j] = false;
         	}
         for(int i=0;i<16;i++)
@@ -274,6 +398,14 @@ public class GUI extends JFrame {
     		return true;
     	return false;
     }
+
+    public boolean inFlagger() {
+    	int dif = (int) Math.sqrt(Math.abs(mx-flaggerCenterX)*Math.abs(mx-flaggerCenterX)+Math.abs(my-flaggerCenterY)*Math.abs(my-flaggerCenterY));
+    	if (dif<35)
+    		return true;
+    	return false;
+    }
+
     public int inBoxX() {
         for (int i = 0; i < 16; i++)
             for (int j = 0; j < 9; j++) {
